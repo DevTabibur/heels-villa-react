f7df10fff5a0006876baff9e1add141a6fb1f032
DevTabibur <tobiburrohman2@gmail.com>
2022-03-29 23:36:43 +0600

    data load and show UI the product compo

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Button, Card, CardImg } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Product = (props) => {
-    const {name, price, pairImage, sideImage} = props.product
-    return (
-        <div>
-            <h1>This is Product{name}</h1>
-        </div>
-    );
+  const { name, price, pairImage, sideImage } = props.product;
+
+  return (
+    <div className="ml-4">
+      <Card style={{width: "18rem"}}>
+        <Card.Img variant="top" src={pairImage} />
+        <Card.Body>
+          <Card.Title>Name: {name}</Card.Title>
+          <Card.Text>
+            Price: ${price}
+          </Card.Text>
+          <Button variant="primary">Add to Cart</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Product;
